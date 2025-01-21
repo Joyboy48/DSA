f83614cedafc68d072a3f28b7bbf2427c1196c4c
Joyboy48 <astitvaarya9589@gmail.com>
2025-01-21 19:37:04 +0530

    ll

--- svr/linkedlist/create.cpp
+++ svr/linkedlist/create.cpp
@@ -20,11 +20,50 @@ class Node{
     }
 };
 
+Node* convertArrToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover =temp;
+    }
+    return head;
+}
+
+int lengthOfLL(Node* head){
+    int count =0;
+    Node* temp = head;
+    while(temp){
+       temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+
+
 
 int main(){
     vector <int> arr = {1,2,3,4,5};
-    Node* y = new Node(arr[0],nullptr);
-    cout<<y<<endl; //address of Node
-    cout<<y->data<<endl;
+
+    Node* head = convertArrToLL(arr);
+    cout<<head->data<<endl;
+
+    //traverse
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+    cout<<lengthOfLL(head)<<endl;
+
+
+
+    // Node* y = new Node(arr[0],nullptr);
+    // cout<<y<<endl; //address of Node
+    // cout<<y->data<<endl;
 
 }
